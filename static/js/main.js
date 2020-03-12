f56352f85510652199c0c6f042787e6eff0f5e90
Antonio Mallia <me@antoniomallia.it>
2020-03-12 19:09:50 +0100

    Improved research page

--- static/js/main.js
+++ static/js/main.js
@@ -15,3 +15,34 @@ jQuery(document).ready(function () {
         $(".resume-link").attr("href", downloadURL);
    });
 });
+jQuery(document).ready(function () {
+    $('.js-cite-modal').click(function(e) {
+        e.preventDefault();
+        let filename = $(this).attr('data-filename');
+        let modal = $('#modal');
+        modal.find('.modal-body code').load(
+            filename,
+            function(response, status, xhr) {
+                if (status == 'error') {
+                    let msg = "Error: ";
+                    $('#modal-error').html(msg + xhr.status + " " + xhr.statusText);
+                } else {
+                    $('.js-download-cite').attr('href', filename);
+                }
+            });
+        modal.modal('show');
+    });
+    $('.js-copy-cite').click(function(e) {
+        e.preventDefault();
+        let range = document.createRange();
+        let code_node = document.querySelector('#modal .modal-body');
+        range.selectNode(code_node);
+        window.getSelection().addRange(range);
+        try {
+            document.execCommand('copy');
+        } catch (e) {
+            console.log('Error: citation copy failed.');
+        }
+        window.getSelection().removeRange(range);
+    });
+});
